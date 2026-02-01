59d70b43bc7c3519dbc193b05c14fd09845b31a7
Luca Zraunig <heimo.zraunig777@gmail.com>
2026-02-01 20:09:08 +0100

    new api

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "client")));
 
-const API_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImVmYjE5MWI4LWIzMzctNDJlYS04YmQ2LTE0OGMwZDJmMjU0ZiIsImlhdCI6MTc2OTc5NjY2NCwic3ViIjoiZGV2ZWxvcGVyLzQ0ODJkYzU1LTk1ZTEtOTM5MC1hMmJlLTk3ZGVlZTc2ODE0ZSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIyMTMuMjI1LjguMTE3Il0sInR5cGUiOiJjbGllbnQifV19.DdRm3jOi0oQMqfySGb0P3Zmwonc83435fENYnEDDbt-T87h3NF7DNCWGfWpMvUw5pX68er3aXhiyc6_uhcc2Sg"; // besser später über .env
- 
+const API_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImIwOWUxM2Q5LTA5MjQtNGVhNi05MDM5LTZkNmNjNDgxMWRlZCIsImlhdCI6MTc2OTk3MjkwNywic3ViIjoiZGV2ZWxvcGVyLzQ0ODJkYzU1LTk1ZTEtOTM5MC1hMmJlLTk3ZGVlZTc2ODE0ZSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3NC4yMjAuNTEuMjUwIl0sInR5cGUiOiJjbGllbnQifV19.chADcrwF4f5Owlh7HOckBIJ2Gt6BYQsqOSKm7TiXA_BgV5-nzkgxeGEb1g_MF-BDdZIQ9jYfmlkk3FPpXFfgQg"
+
 app.get("/player/:tag", async (req, res) => {
     try {
         const tag = `%23${req.params.tag.replace("#", "")}`;
